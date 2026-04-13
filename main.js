00d9be86573471600c479cca634ce282e98d35d7
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-04-13 11:58:06 +0500

    kod yozish

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-const wrappper = document.getElementById("container");
+const wrapper = document.getElementById("container");
+
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(data => {
+
         render(data);
     })
-    .catch(error => console.error("Xatolik yuz berdi", error));
-function render(user) {
+    .catch(error => console.error("Xatolik yuz berdi:", error));
+
+function render(users) {
     const cards = users.map(el => `
-    <div class ="card>
-        <h1>${el.name}</h1>
-        <p>${el.username}</p>
-        <a href="mailto:${el.email}">${el.email}</a>
-    </div>
-        `).join("");
-        wrappper.innerHTML = cards
+                <div class="card">
+                   <h1>${el.name}</h1>
+                    <p>@${el.username}</p>
+                   <a href="mailto:${el.email}">${el.email}</a>
+                </div>
+            `).join(""); 
+            
+    wrapper.innerHTML = cards;
 }
