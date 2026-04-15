dc33a1b19eb4ced34079a1e0c9366fec836b82aa
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-04-15 20:59:46 +0500

    function render

--- main.js
+++ main.js
@@ -8,3 +8,14 @@ fetch('https://jsonplaceholder.typicode.com/users')
     })
     .catch(error => console.error("Xatolik yuz berdi:", error));
 
+function render(users) {
+    const cards = users.map(el => `
+                <div class="card">
+                   <h1>${el.name}</h1>
+                    <p>@${el.username}</p>
+                   <a href="mailto:${el.email}">${el.email}</a>
+                </div>
+            `).join(""); 
+            
+    wrapper.innerHTML = cards;
+}
