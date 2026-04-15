730401c80155cb1c37d480b776b22206639ea70c
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-04-15 20:57:59 +0500

    fetch

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 const wrapper = document.getElementById("container");
 
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => {
+
+        render(data);
+    })
